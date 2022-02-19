21cf8b9404b9cce3795b49cc08b851facc524110
wsmperquin <perquin@bakspa.nl>
2022-02-19 12:47:46 +0100

    wrote a small testscript so users can see the changes made in the sepreate sessionhandling database

--- testScript.php
+++ testScript.php
@@ -1,6 +1,11 @@
 <?php
+include_once('../dbconfig.php');
 include_once('classes/yottadotssessionhandler.php');
 $handler = new yottadotssessionhandler();
 //and now you can start your sessions. all sessiondata is save in, updated and collected from in a seperate database
 session_start();
+$_SESSION['testdata'] = 'when you look in your database this sentence can be found in it';
+print_r(session_id());
+
+?>
 
