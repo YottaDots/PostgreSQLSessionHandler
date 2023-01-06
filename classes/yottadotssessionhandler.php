fed25363b2e5c09e1fcf399c5de930221432d1a6
wsmperquin <perquin@bakspa.nl>
2023-01-06 18:28:01 +0100

    updated to PHP version 8.1.* it's also still compatible with PHP 7.4.*

--- classes/yottadotssessionhandler.php
+++ classes/yottadotssessionhandler.php
@@ -6,100 +6,112 @@
  * https://github.com/dominicklee/PHP-MySQL-Sessions/blob/master/mysql.sessions.php
  *
  */
-class yottadotssessionhandler implements SessionHandlerInterface {
-    //SessionUpdateTimestampHandlerInterface
-    /*
-     * Datacolumns in the database:
-     * idsessiondata (compatible with session_id
-     * sessiondata
-     * lastchanged (timestamp with timezone)
-     *
-     * session_id
-     */
-    private $dbconnection = '';
-    public function __construct() {
-        //open databse connection
-        $this->dbconnection = pg_connect("host=".DBHOSTSESSION." dbname=".DBNAMESESSION." user=".DBUSERNAMESESSION." password=".DBPASSWORDSESSION."");
-        session_set_save_handler (
-            array($this, 'open')
-            ,array($this, 'close')
-            ,array($this, 'read')
-            ,array($this, 'write')
-            ,array($this, 'destroy')
-            ,array($this, 'gc')
-            ,array($this, 'create_sid')
-        // ,array($this, 'validate_sid')
-        // ,array($this, 'update_timestamp')
-        );
-    }
-    public function close(){
+class yottadotssessionhandler implements SessionHandlerInterface
+{
+	private $savePath;
+	private $dbconnection = '';
+	public function __construct() {
+		//open databse connection
+		$this->dbconnection = pg_connect("host=".DBHOSTSESSION." dbname=".DBNAMESESSION." user=".DBUSERNAMESESSION." password=".DBPASSWORDSESSION."");
+		session_set_save_handler (
+			array($this, 'open')
+			,array($this, 'close')
+			,array($this, 'read')
+			,array($this, 'write')
+			,array($this, 'destroy')
+			,array($this, 'gc')
+			,array($this, 'create_sid')
+		);
+	}
+	public function open($sessionSavePath, $sessionName) : bool
+	{
+		// return value should be true for success or false for failure
+		$this->savePath = $sessionSavePath;
+		return true;
+	}
+    public function close() : bool
+    {
         // return value should be true for success or false for failure
         // ...
         //close the database connection)
         if(pg_close($this -> dbconnection)){
+//            echo 'Session has closed'.PHP_EOL;
             return true;
         } else {
+//            echo 'Session close went wrong'.PHP_EOL;
             return false;
         }
     }
-    public function destroy($sessionId){
+	#[\ReturnTypeWillChange]
+	public function read($sessionId){
+		// return value should be the session data or an empty string
+		$qSelect = 'SELECT sessiondata FROM sessiondata WHERE idsessiondata=\''.pg_escape_string($this->dbconnection,$sessionId).'\'';
+		$qResult = pg_query($this->dbconnection, $qSelect);
+		if($qResult) {
+			$aResult = pg_fetch_all($qResult);//pg_fetch_all returns an array with values or false when nothing has been found
+			if($aResult) {
+				if(count($aResult) > 0 ) {
+//                    echo  'Session read1'.PHP_EOL;
+//                    echo '$sessionId'.$sessionId.PHP_EOL;
+//	                print_r($aResult);
+					return (string)$aResult[0]['sessiondata'];
+				} else {
+//                    echo  'Session read2'.PHP_EOL;
+//                    echo  '$aResult empty'.PHP_EOL;
+//                    echo '$sessionId'.$sessionId.PHP_EOL;
+					return '';
+				}
+			} else {
+//                echo  'Session read3'.PHP_EOL;
+//                echo  '$aResult empty or nothing was found'.PHP_EOL;
+//                echo '$sessionId'.$sessionId.PHP_EOL;
+				return '';
+			}
+		} else {
+//            echo  'Session read4'.PHP_EOL;
+//            echo  '$aResult empty because an error accured'.PHP_EOL;
+//            echo '$sessionId'.$sessionId.PHP_EOL;
+			return '';
+		}
+	}
+
+	public function write($sessionId, $sessionData): bool
+	{
+		// return value should be true for success or false for failure
+		$qUpdate = 'UPDATE sessiondata
+            SET sessiondata = \''.pg_escape_string($sessionData).'\'
+            , lastchanged = now()
+            WHERE idsessiondata=\''.pg_escape_string($sessionId).'\'';
+		$qResult = pg_query($this->dbconnection, $qUpdate);
+		if($qResult) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+    public function destroy($sessionId):bool
+    {
         // return value should be true for success or false for failure
         // ...
         //delete the session ID
         $qDelete = ' DELETE FROM sessiondata WHERE idsessiondata=\''.pg_escape_string($sessionId).'\'';
         if(pg_query($this -> dbconnection, $qDelete)) {
+//            echo 'Session has destroyed: '.$sessionId.PHP_EOL;
             return true;
         } else {
+//            echo '$qDelete'.$qDelete.PHP_EOL;
             return false;
         }
     }
+	#[\ReturnTypeWillChange]
     public function gc($maximumLifetime){
         // return value should be true for success or false for failure
+//	    echo 'gc '.$maximumLifetime.PHP_EOL;
         // ...
         return true;
     }
-    public function open($sessionSavePath, $sessionName){
-        // return value should be true for success or false for failure
-        $nr=session_id();
-//	    echo 'session_id'.strlen($nr).PHP_EOL;
-//	    echo 'sessionid'.$nr.PHP_EOL;
-//        echo '$sessionSavePath : '.$sessionSavePath.PHP_EOL;
-//        echo '$sessionName: '.$sessionName.PHP_EOL;
-        return true;
-    }
-    public function read($sessionId){
-        // return value should be the session data or an empty string
-        $qSelect = 'SELECT sessiondata FROM sessiondata WHERE idsessiondata=\''.pg_escape_string($sessionId).'\'';
-        $qResult = pg_query($this->dbconnection, $qSelect);
-        if($qResult) {
-            $aResult = pg_fetch_all($qResult);//pg_fetch_all returns an array with values or false when nothing has been found
-            if($aResult) {
-                if(count($aResult) > 0 ) {
-                    return $aResult[0]['sessiondata'];
-                } else {
-                    return '';
-                }
-            } else {
-                return '';
-            }
-        } else {
-            return '';
-        }
-    }
-    public function write($sessionId, $sessionData){
-        // return value should be true for success or false for failure
-        $qUpdate = 'UPDATE sessiondata
-            SET sessiondata = \''.pg_escape_string($sessionData).'\'
-            , lastchanged = now()
-            WHERE idsessiondata=\''.pg_escape_string($sessionId).'\'';
-        $qResult = pg_query($this->dbconnection, $qUpdate);
 
-        if($qResult) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+
 
     public function create_sid()
     {
@@ -107,6 +119,7 @@ class yottadotssessionhandler implements SessionHandlerInterface {
         $qResult = pg_query($this->dbconnection, $qSelect);
         if($qResult) {
             $aResult = pg_fetch_all($qResult);
+//            echo 'session id created '.$aResult[0]['createuniquesessionid'];
             return $aResult[0]['createuniquesessionid'];
         } else {
             return '';
