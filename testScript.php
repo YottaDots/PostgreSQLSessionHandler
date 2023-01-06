4b249e6fbfec6e4bafaeadb694f0f6f51072da43
wsmperquin <perquin@bakspa.nl>
2023-01-06 18:37:33 +0100

    small rebaseing of stuff

--- testScript.php
+++ testScript.php
@@ -3,6 +3,7 @@ include_once('../dbconfig.php');
 include_once('classes/yottadotssessionhandler.php');
 $handler = new yottadotssessionhandler();
 //and now you can start your sessions. all sessiondata is save in, updated and collected from in a seperate database
+session_set_save_handler($handler, true);
 session_start();
 $_SESSION['testdata'] = 'when you look in your database this sentence can be found in it';
 print_r(session_id());
